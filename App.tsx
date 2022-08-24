b593337228708b2fd518db75eb03a605fd6fce46
Danubio de Araújo <danubio.bwm@gmail.com>
2022-08-23 23:34:34 -0300

    Icons

--- App.tsx
+++ App.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 
 import { ThemeProvider } from "styled-components";
-import theme from './src/global/styles/theme';
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import theme from "./src/global/styles/theme";
 
 import { StatusBar } from "expo-status-bar";
 import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
   return (
     <>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme} >
         <StatusBar style="auto" />
         <Dashboard />
       </ThemeProvider>
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import { Container, Title } from './styles';
+import React from "react";
 
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+  Icon
+} from "./styles";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export function Dashboard() {
   return (
-    <Container>
-      <Title>Dashboard</Title>
-    </Container>
+    <>
+      <Container>
+        <Header>
+          <UserWrapper>
+            <UserInfo>
+              <Photo source={{ uri: "https://github.com/danubiobwm.png" }} />
+              <User>
+                <UserGreeting>Olá,</UserGreeting>
+                <UserName>Danubio</UserName>
+              </User>
+            </UserInfo>
+            <Icon name="power"/>
+          </UserWrapper>
+        </Header>
+      </Container>
+    </>
   );
 }
-
