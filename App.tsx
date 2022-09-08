7a40b2c6101d99e3b9d6d6a35d1a32a935215987
Danubio de Araújo <danubio.bwm@gmail.com>
2022-09-08 17:19:22 -0300

    Resolução do SpashScreen

--- App.tsx
+++ App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
+import React, { useEffect, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from 'styled-components';
 import { Dashboard } from './src/screens/Dashboard';
+import { useFonts } from 'expo-font';
 import {
-  useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 import theme from './src/global/styles/theme';
+import { LogBox } from 'react-native';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_700Bold
   });
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  LogBox.ignoreLogs([
+    'Found screens with the same name nested inside one another',
+  ]);
+
+  useEffect(() => {
+    //previse que a SplashScreen saia de tela
+    const showSplashScreen = async () => {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    showSplashScreen();
+  }, []);
+
+  useEffect(() => {
+    //quando as fontes carregarem, desmonta a splash
+    const hideSplashScreen = async () => {
+      await SplashScreen.hideAsync();
+    }
+    if (fontsLoaded) {
+      setAppIsReady(true);
+      hideSplashScreen();
+    }
+  }, [fontsLoaded]);
+  
+  if (!appIsReady) {
+    return null;
   }
 
   return (
