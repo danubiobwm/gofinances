9e8dde96758f52d9582009ea3cfa7b132c16187b
Danubio de Araújo <danubio.bwm@gmail.com>
2022-09-19 21:32:31 -0300

    Button Selection

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../../components/Button";
+import { TransactionTypeButton } from "../../components/TransactionTypeButton";
 import { Input } from "../../components/Input";
-import { Container, Header, Title, Form, Fields } from "./styles";
-
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionTypes,
+} from "./styles";
 
 export function Register() {
+  const [transactionType, setTransactionType] = useState("");
+
+  const handleTransactionsTypeSelect = (type: "up" | "down") => {
+    setTransactionType(type);
+  };
+
   return (
     <Container>
       <Header>
@@ -14,6 +27,20 @@ export function Register() {
         <Fields>
           <Input placeholder="Nome" />
           <Input placeholder="Preço" />
+          <TransactionTypes>
+            <TransactionTypeButton
+              title="Income"
+              type="up"
+              onPress={() => handleTransactionsTypeSelect("up")}
+              isActive={transactionType === "up"}
+            />
+            <TransactionTypeButton
+              title="Outcome"
+              type="down"
+              onPress={() => handleTransactionsTypeSelect("down")}
+              isActive={transactionType === "down"}
+            />
+          </TransactionTypes>
         </Fields>
         <Button title="Enviar" />
       </Form>
