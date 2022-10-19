94b857e5f6023706ad561c9d59da6f2e3d15d8e7
Danubio de Araújo <danubio.bwm@gmail.com>
2022-10-18 21:44:10 -0300

    tabNavigations

--- App.tsx
+++ App.tsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import * as SplashScreen from 'expo-splash-screen';
-import { ThemeProvider } from 'styled-components';
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
-import { CategorySelect } from './src/screens/CategorySelect';
-import { useFonts } from 'expo-font';
+import React, { useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { ThemeProvider } from "styled-components";
+import { useFonts } from "expo-font";
 import {
   Poppins_400Regular,
   Poppins_500Medium,
-  Poppins_700Bold
-} from '@expo-google-fonts/poppins';
-import theme from './src/global/styles/theme';
-import { LogBox } from 'react-native';
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import theme from "./src/global/styles/theme";
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "./src/routes/app.routes";
+
+import { LogBox } from "react-native";
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
-    Poppins_700Bold
+    Poppins_700Bold,
   });
 
   LogBox.ignoreLogs([
-    'Found screens with the same name nested inside one another',
+    "Found screens with the same name nested inside one another",
   ]);
 
   useEffect(() => {
     //previse que a SplashScreen saia de tela
     const showSplashScreen = async () => {
       await SplashScreen.preventAutoHideAsync();
-    }
+    };
     showSplashScreen();
   }, []);
 
@@ -37,20 +37,22 @@ export default function App() {
     //quando as fontes carregarem, desmonta a splash
     const hideSplashScreen = async () => {
       await SplashScreen.hideAsync();
-    }
+    };
     if (fontsLoaded) {
       setAppIsReady(true);
       hideSplashScreen();
     }
   }, [fontsLoaded]);
-  
+
   if (!appIsReady) {
     return null;
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
-import { Modal } from "react-native";
+import { Modal, TouchableWithoutFeedback, Keyboard, Alert } from "react-native";
+
+import { useForm } from "react-hook-form";
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import { Button } from "../../components/Button";
+import { InputForm } from "../../components/InputForm";
 import { TransactionTypeButton } from "../../components/TransactionTypeButton";
 import { CategorySelectButton } from "../../components/CategorySelectButton";
-import { Input } from "../../components/Input";
-
 import { CategorySelect } from "../CategorySelect";
 
 import {
@@ -17,14 +20,30 @@ import {
   TransactionTypes,
 } from "./styles";
 
-export function Register() {
+interface FormData {
+  name: string;
+  amount: string;
+}
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('Nome é obrigatório'),
+  amount: Yup.number()
+  .typeError('Informe um valor númerico')
+  .positive('valor tem que ser positivo')
+  .required('O valor é obrigatório')
+});
+
+export function Register() {
   const [CategoryModalOpen, setCategoryModalOpen] = useState(false);
   const [transactionType, setTransactionType] = useState("");
 
   const [category, setCategory] = useState({
     key: "category",
     name: "category",
+  });
+
+  const { control, handleSubmit, formState:{errors} } = useForm({
+    resolver: yupResolver(schema)
   });
 
   const handleTransactionsTypeSelect = (type: "up" | "down") => {
@@ -36,46 +55,77 @@ export function Register() {
   };
 
   const handleOpenSelectCategoryModal = () => {
-    setCategoryModalOpen(true); 
+    setCategoryModalOpen(true);
+  };
+
+  const handleRegister = (form: FormData) => {
+    if(!transactionType)
+      return Alert.alert('Selecione o tipo da transação');
+    
+      if(category.key === 'category')
+      return Alert.alert('Selecione o tipo da categoria');
+
+    const data = {
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key,
+    };
+    console.log(data);
   };
 
   return (
-    <Container>
-      <Header>
-        <Title>Cadastro</Title>
-      </Header>
-      <Form>
-        <Fields>
-          <Input placeholder="Nome" />
-          <Input placeholder="Preço" />
-          <TransactionTypes>
-            <TransactionTypeButton
-              title="Income"
-              type="up"
-              onPress={() => handleTransactionsTypeSelect("up")}
-              isActive={transactionType === "up"}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Container>
+        <Header>
+          <Title>Cadastro</Title>
+        </Header>
+        <Form>
+          <Fields>
+            <InputForm
+              placeholder="Nome"
+              name="name"
+              control={control}
+              autoCapitalize="sentences"
+              autoCorrect={false}
+              error={errors.name && errors.name.message}
             />
-            <TransactionTypeButton
-              title="Outcome"
-              type="down"
-              onPress={() => handleTransactionsTypeSelect("down")}
-              isActive={transactionType === "down"}
+            <InputForm
+              placeholder="Preço"
+              name="amount"
+              control={control}
+              keyboardType="numeric"
+              error={errors.amount && errors.amount.message}
             />
-          </TransactionTypes>
-          <CategorySelectButton 
-          title={category.name}
-          onPress={handleOpenSelectCategoryModal}
+            <TransactionTypes>
+              <TransactionTypeButton
+                title="Income"
+                type="up"
+                onPress={() => handleTransactionsTypeSelect("up")}
+                isActive={transactionType === "up"}
+              />
+              <TransactionTypeButton
+                title="Outcome"
+                type="down"
+                onPress={() => handleTransactionsTypeSelect("down")}
+                isActive={transactionType === "down"}
+              />
+            </TransactionTypes>
+            <CategorySelectButton
+              title={category.name}
+              onPress={handleOpenSelectCategoryModal}
+            />
+          </Fields>
+          <Button title="Enviar" onPress={handleSubmit(handleRegister)} />
+        </Form>
+        <Modal visible={CategoryModalOpen}>
+          <CategorySelect
+            category={category}
+            setCategory={setCategory}
+            closeSelectCategory={handleCloseSelectCategoryModal} 
           />
-        </Fields>
-        <Button title="Enviar" />
-      </Form>
-      <Modal visible={CategoryModalOpen}>
-        <CategorySelect
-          category={category}
-          setCategory={setCategory}
-          closeSelectCategory={handleCloseSelectCategoryModal}
-        />
-      </Modal>
-    </Container>
+        </Modal>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
